3e67e2cb315c69481208d5010301330c74074aac
Ryosuke-Shigi <adalovelace6365@gmail.com>
2021-12-27 23:13:48 +0900

    AWS

--- laravel/app/Http/Controllers/API/createController.php
+++ laravel/app/Http/Controllers/API/createController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use Storage;
+
 
 class createController extends Controller
 {
@@ -16,5 +18,18 @@ class createController extends Controller
     public function startDelete(REQUEST $request){
         dump($request);
         return 1;
+    }
+
+    //画像保存　こちらで保存したPATHを返す
+    public function createPict(REQUEST $request){
+        if($request->pict->isValid()){
+/*             dump($request->pict);
+            dump($request->name);
+            dump($request->mimeType);
+ */        }
+        $image=$request->file('pict');
+        dump($image);
+        $responce=Storage::disk('s3')->put('/pict',$image,'public-read');
+        return $responce;
     }
 }
